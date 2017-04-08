213f9c5d3fcc7694fe7463c0e39d2ce20390ab02
SOELexicon <CraigWright@purestaff.local>
2017-04-08 23:04:54 +0100

    0.1.31 64 bit
    Added GitHub API
    Added Check For Updates On Load using GithubAPI
    Added Resume Last Plot Button In Plot Configurator. (Only Works for
    xPlotter)
    Fixed Bug with GPU Plotter- when application isnt stored on C it still
    tried to find the application on C.
    Fixed some more grammar

--- XPlotter/AstroCpuStart.bat
+++
@@ -1,5 +0,0 @@
-@setlocal
-@cd /d %~dp0
-XPlotter_avx.exe -id 1 -sn 1819154435 -n 204800 -t 7 -path C://Burst/plots/ -mem 6G
-XPlotter_avx.exe -id 1 -sn 1819359236 -n 204800 -t 7 -path C://Burst/plots/ -mem 6G
-@pause
--- XPlotter/RunThisAsAdmin - Copy.bat
+++
@@ -1,4 +0,0 @@
-@setlocal
-@cd /d %~dp0 
-XPlotter_avx.exe -id 6678077075331343979 -sn 1809754113 -n 4700160 -t 4 -path R:\Burst\plots -mem 6G
-@pause
