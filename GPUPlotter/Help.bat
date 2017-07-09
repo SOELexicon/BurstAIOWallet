9f85a112da13ff0b36df9e26127031f739743218
MSFTserver <sawyerlemmo@gmail.com>
2017-07-09 02:30:36 -0700

    default edits

---
+++ GPUPlotter/Help.bat
@@ -0,0 +1,2 @@
+gpuPlotGenerator.exe
+@pause
--- XPlotter/run.bat
+++ XPlotter/run.bat
@@ -1,2 +1,2 @@
-XPlotter.exe -id 17930413153828766298 -sn 200000000 -n 0 -t 6 -path L:\plots
+XPlotter.exe -id 17930413153828766298 -sn 0 -n 0 -t 6 -path L:\plots -mem 6G
 pause
